2fe41577697062724019302fef3f1f0047260f01
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-09-08 00:18:03 +0700

    Create pasang.sh

---
+++ img/pasang.sh
@@ -0,0 +1,13 @@
+ apt install g++
+apt-get install curl jq git ossp-uuid ncurses-utils lzma nodejs
+apt-get install nodejs-lts python
+npm -g i bash-obfuscate
+pip install rich
+pip install rich-cli
+git clone https://github.com/YaddyKakkoii/ydx
+cd ydx
+apt install npm
+bash bash.setup build && clear
+clear
+clear
+cd
